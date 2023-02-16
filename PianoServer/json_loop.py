10a346992da706fb4b1b855fa33e7ac23835b8db
steven65026502 <steven65026502@gmail.com>
2023-02-16 23:08:30 +0800

    json

--- PianoServer/json_loop.py
+++ PianoServer/json_loop.py
@@ -3,16 +3,11 @@ import os
 import json
 import time
 
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/master
 class json_list(Dataset):
     def __init__(self, root_dir, label_dir):
         self.root_dir = root_dir
         self.label_dir = label_dir
         self.path = os.path.join(self.root_dir, self.label_dir)
-<<<<<<< HEAD
         self.num_files = len(os.listdir(self.path))
 
     def __getitem__(self, idx):
@@ -27,20 +22,6 @@ class json_list(Dataset):
 
     def __len__(self):
         return self.num_files
-=======
-        self.json_path = os.listdir(self.path)
-
-    def __getitem__(self, idx):
-        json_name = self.json_path[idx]
-        json_item_path = os.path.join(self.root_dir, self.label_dir, json_name)
-        with open(json_item_path) as f:
-            data = json.load(f)
-        label = label_dir
-        return data, label
-
-    def __len__(self):
-        return len(self.json_path)
->>>>>>> origin/master
 
 
 root_dir = "C:\\Users\\謝向嶸\\Desktop\\專題用\\PIANO"
@@ -51,8 +32,4 @@ for i, (data, label) in enumerate(pr_dataset):
     print(f"Item {i}:")
     print(f"Data: {data}")
     print(f"Label: {label}")
-<<<<<<< HEAD
     # time.sleep(0.1)
-=======
-    time.sleep(0.1)
->>>>>>> origin/master
